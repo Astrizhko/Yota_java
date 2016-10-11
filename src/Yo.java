d78dc90e3dccf7c2081f1db852c627cf686738a8
AStrizhko <astrizhko@gmail.com>
2016-10-11 12:37:32 +0300

    Initial commit

---
+++ src/Yo.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+public class Yo {
+    public static void main(String[] args) throws IOException {
+       char c1 = 96;
+        while (c1 <= 121) {
+            System.out.println(++c1);
+        }
+        char c2 = 1039;
+        while (c2 <= 1102) {
+            System.out.println(++c2);
+        }
+    }
+}
