53b8ab1b69af3dc7b1220a8855267e2ef4429257
AStrizhko <astrizhko@gmail.com>
2016-10-13 16:33:03 +0300

    1

---
+++ src/Home_2.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Strizhko on 13.10.2016.
+ */
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Home_2 {
+    public static void main(String[] args) {
+      //  System.out.println("penis");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите имя: ");
+        String name = in.nextLine();
+        System.out.println("Привет: " + name);
+
+    }
+}
