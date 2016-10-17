80009a6431f59bd2a57c9a2583ba4b82058ff82b
AStrizhko <astrizhko@gmail.com>
2016-10-17 17:54:14 +0300

    Home_work_2

--- src/Home_2_5.java
+++ src/Home_2_5.java
@@ -3,6 +3,16 @@
  */
 public class Home_2_5 {
     public static void main(String[] args) {
+        int s = 0, p = 1;
+        for (int i = 0; i < args.length; i++) {
+            int ss = Integer.parseInt(args[i]);
+            s += ss;
+            p *= ss;
+           // System.out.println(args[i]); наш ссаный масив
+        }
+        System.out.println("summ = " + s);
+        System.out.println("proizv = " + p);
+
 
     }
 }
