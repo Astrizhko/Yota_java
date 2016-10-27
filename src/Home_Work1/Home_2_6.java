58f6b765bed9d27d6bbefe75ab86aa6c00cdc6ef
AStrizhko <astrizhko@gmail.com>
2016-10-27 18:40:47 +0300

    Home_work_1

--- src/Home_Work1/Home_2_6.java
+++ src/Home_Work1/Home_2_6.java
@@ -14,28 +14,40 @@ public class Home_2_6 {
         int numArr[] = new int[str.length];  //перепиливаем в масив интов
         for (int i = 0; i < str.length; i++) {
             numArr[i] = Integer.parseInt(str[i]);
-            System.out.println(numArr[i]);
+            System.out.println(numArr[i]);  //выводим масив интов ?
         }
+        System.out.println("Задание 6.2");
         int max = numArr[0], min = numArr[0];
         for (int i = 0; i < numArr.length; i++) {
             if (max < numArr[i])
                 max = numArr[i];
             if (min > numArr[i])
                 min = numArr[i];
-            System.out.println("MAx_value = " + max);
-            System.out.println("MIn_value = " + min);
-
-       /* for (String i : str){
-            byte[] a = i.getBytes();
-            byte[] b = i.getBytes();
-            Arrays.sort(b);
-            if (Arrays.equals(a,b)){
-                System.out.println(i);
-                break;
-            }
         }
-*/
+        System.out.println("MAx_value = " + max);  //максимальное число из массива
+        System.out.println("MIn_value = " + min);  //минимальное число из массива
 
+        System.out.println("задание 6.3");
+        for (int j = 0; j < numArr.length; j++) {
+           if  ((numArr[j]%3==0)|(numArr[j]%9==0))System.out.println(numArr[j] + "  " + " делится на 3 или на 9 ");  //числа которые делятся на 3 или на 9
+          else System.out.println(numArr[j] + "  " + " не делится на 3 или на 9 без остатка");
         }
+        System.out.println("Задание 6.4");
+
+        for (int j = 0; j < numArr.length; j++) {
+            if  ((numArr[j]%5==0)&(numArr[j]%7==0))System.out.println(numArr[j] + "  " + " делится на 5 и на 7 ");  //числа которые делятся на 5 и на 7
+            else System.out.println(numArr[j] + "  " + "Не делится на 5 и 7");
+        }
+        System.out.println("Задание 6.5 Все трехзначные числа, в десятичной записи которых нет одинаковых цифр");
+
+        for (int x = 0; x < numArr.length; x++) {
+            if  (((numArr[x]>=100)&(numArr[x]<=999))&(numArr.toString().charAt(0) != numArr.toString().charAt(1) && numArr.toString().charAt(0) != numArr.toString().charAt(2) && numArr.toString().charAt(1) != numArr.toString().charAt(2)
+                    ))System.out.println(numArr[x] + "  " + " трехзначное нет одинаковых цифр ");  //Все трехзначные числа, в десятичной записи которых нет одинаковых цифр
+
+            else System.out.println(numArr[x] + "  " + "Не трехзначное или одинаковые цифры");
+        }
+        System.out.println("Задание 6.6");
+
+
     }
 }
